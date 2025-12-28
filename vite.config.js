4618de568da5ddf5e9777f6b654193104201aed1
ignatjevvv <k.ignatjevvv@gmail.com>
2025-12-28 19:57:37 +0200

    add base url vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', // Автоматически обновляет сервис-воркер
+      manifest: {
+        name: 'One Fine Day - Передбачення',
+        short_name: 'OneFineDay',
+        description: 'Твоє щоденне передбачення від AI',
+        theme_color: '#ffffff', // Цвет верхней панели в браузере
+        background_color: '#ffffff', // Цвет фона при запуске
+        display: 'standalone', // Запуск как отдельное приложение (без строки адреса)
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+  base: "/one-fine-day/",
+});
